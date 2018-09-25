0951730e3ab2bb3650b2b36097dba6bc5ac33656
Woong <1406493495@qq.com>
2018-09-25 17:42:35 +0800

    manipulator

---
+++ devel/share/gennodejs/ros/open_manipulator_msgs/msg/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let State = require('./State.js');
+let JointPosition = require('./JointPosition.js');
+let KinematicsPose = require('./KinematicsPose.js');
+
+module.exports = {
+  State: State,
+  JointPosition: JointPosition,
+  KinematicsPose: KinematicsPose,
+};
